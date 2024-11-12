22ab34149a4fcc6e6009fc3392243acab10acb72
Carlos Eduardo-PC_trabalho <eduardo201184@hotmail.com>
2024-11-12 17:26:36 -0300

    feature: Get on route /Posts done, missing list of posts

--- client/src/pages/page-forum/forum.tsx
+++ client/src/pages/page-forum/forum.tsx
@@ -1,6 +1,20 @@
+import { getPosts } from "@/services/conexao";
+import { useEffect } from "react";
 import NavBar from "./navbar";
 
 export default function Forum() {
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const fetchedPosts = await getPosts();
+        console.log(fetchedPosts);  // Agora deve exibir os dados corretos
+      } catch (error) {
+        console.log('Erro ao buscar motos:', error);
+      }
+    };
+    fetchPosts();
+  }, []);
   return (
     <div>
       <NavBar />
---
+++ client/src/services/conexao.ts
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+const url = 'http://localhost:3000'
+
+export const getPosts = async () =>{
+    const response = await axios.get(`${url}/api/posts`)
+    return response.data
+}
