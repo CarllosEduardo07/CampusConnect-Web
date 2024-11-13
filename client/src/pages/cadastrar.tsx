229e033ec11bcce2a304bad70b9a1cfe81e33cbd
Carlos Eduardo-PC_trabalho <eduardo201184@hotmail.com>
2024-11-13 15:22:16 -0300

    style: adjusting the cadastro component of the cadastro route

--- client/src/pages/cadastrar.tsx
+++ client/src/pages/cadastrar.tsx
@@ -1,53 +1,43 @@
-import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 
 export default function Cadastrar() {
   return (
-    <body className="flex justify-center items-center min-h-screen">
-      <section className="w-full h-screen bg-bannerImg bg-no-repeat bg-cover bg-bottom flex justify-center items-center">
-        <section className="w-full h-full flex justify-center items-center">
-          <article className="bg-white p-8 rounded-3xl w-1/3">
-            <img
-              src="src/assets/campusconnectlogo.svg"
-              alt="logocampusconnect"
-              className="w-40 mx-auto"
-            />
+    <section className='w-full h-screen bg-bannerImg bg-no-repeat bg-cover bg-bottom flex justify-center items-center'>
+      <section className='w-full h-full flex justify-center items-center'>
+        <article className='bg-white p-8 rounded-3xl w-1/3'>
+          <img src='src/assets/campusconnectlogo.svg' alt='logocampusconnect' className='w-40 mx-auto' />
 
-            <article className="space-y-5 flex flex-col">
-              <Input type="name" placeholder="Nome Completo" />
-              <Input type="email" placeholder="Email" />
-              <Input type="name" placeholder="Matricula da instituição" />
-              <Input type="password" placeholder="Criar Senha" />
+          <form className='space-y-5 flex flex-col'>
+            <Input type='text' placeholder='Nome Completo' required />
+            <Input type='email' placeholder='Email' required />
+            <Input type='number' placeholder='Cpf' required />
+            <Input type='text' placeholder='Matricula da instituição' required />
+            <Input type='password' placeholder='Criar Senha' required />
 
-              <Button
-                type="submit"
-                className="bg-purple-600 hover:bg-purple-700"
-              >
-                Criar Conta
-              </Button>
+            <Button type='submit' className='bg-purple-600 hover:bg-purple-700'>
+              Criar Conta
+            </Button>
 
-              <span className="mx-auto text-purple-700">
-                Já tem uma conta?{" "}
-                <strong>
-                  <a href="/p">login</a>
-                </strong>
+            <span className='mx-auto text-purple-700'>
+              <a href={`/`} className='hover:underline'>
+                Já tem uma conta? <strong>Login</strong>
+              </a>
+            </span>
+            <span className='mx-auto text-purple-700 text-center'>
+              <strong>Junte-se</strong> a mais de <strong>2M</strong> de usuários <br />
+              campus connect!
+              <span className='flex justify-center pt-3 h-10'>
+                <img src='src/assets/Avatar group.png' alt='groupavatar' />
               </span>
-              <span className="mx-auto text-purple-700 text-center">
-                <strong>Junte-se</strong> a mais de <strong>2M</strong> de
-                usuários <br />
-                campus connect!
-                <span className="flex justify-center pt-3 h-10">
-                  <img src="src/assets/Avatar group.png" alt="groupavatar" />
-                </span>
-              </span>
-            </article>
-          </article>
+            </span>
+          </form>
+        </article>
 
-          <figure className="w-2/5 ml-5">
-            <img src="src/assets/parttwocadastro.png" alt="cadastroimg" />
-          </figure>
-        </section>
+        <figure className='w-2/5 ml-5'>
+          <img src='src/assets/parttwocadastro.png' alt='cadastroimg' />
+        </figure>
       </section>
-    </body>
+    </section>
   );
 }
